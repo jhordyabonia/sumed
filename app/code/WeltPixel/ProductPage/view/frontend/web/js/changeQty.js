f30a2b2efa1e3b8d5d6ffe9e4fcb9aee82658395
martinboca <ebocanegra@fi.uba.ar>
2022-08-23 11:17:24 -0300

    added Pearl Theme (weltpixel guide)

---
+++ app/code/WeltPixel/ProductPage/view/frontend/web/js/changeQty.js
@@ -0,0 +1,26 @@
+define([
+    'ko',
+    'uiComponent',
+    'jquery'
+], function (ko, Component, $) {
+    'use strict';
+    return Component.extend({
+        initialize: function () {
+            this._super();
+            this.qty = ko.observable($(this.qtyInput).val() * 1);
+        },
+        decreaseQty: function() {
+            var newQty = this.qty() - 1;
+            if (newQty < 1)
+            {
+                newQty = 1;
+            }
+            this.qty(newQty);
+        },
+        increaseQty: function() {
+            var newQty = this.qty() + 1;
+            this.qty(newQty);
+        }
+    });
+
+});
