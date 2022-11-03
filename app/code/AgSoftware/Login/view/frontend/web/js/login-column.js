3a0675be0b442a4dab06a636996af9beeac0b6f6
Jhordy Abonia <jhordy.abonia@gmail.com>
2022-11-03 18:57:58 -0500

    fix

--- app/code/AgSoftware/Login/view/frontend/web/js/login-column.js
+++ app/code/AgSoftware/Login/view/frontend/web/js/login-column.js
@@ -9,11 +9,21 @@ define([
                customer:CustomerData.get('customer')
            },
            getFullName: function () {
-               try {
-                   return this.customer().fullname;
-               }catch (e) {
-                   return false; //eslint-disable-line eqeqeq
+               if(location.pathname.indexOf("checkout") >= 0){
+                   return false;
                }
+               try {
+                   let out = this.customer().fullname;
+                   if(out) {
+
+                       $('#tab-mini-cart.showcart').show();
+                       $('#login-column.display-sidebar-login').show();
+                       $('body').addClass('logged');
+                       return out;
+                   }
+               }catch (e) {}
+               $('body').removeClass('logged');
+               return false; //eslint-disable-line eqeqeq
            }
        }
    )
