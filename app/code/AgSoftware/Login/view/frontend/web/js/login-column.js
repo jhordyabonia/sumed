5e7dcdda24cd5b7b088ba599f279a0a8307f3fe5
Javier Monzon <javier.monzon@infracommerce.lat>
2022-11-11 12:32:17 -0500

    change link31

--- app/code/AgSoftware/Login/view/frontend/web/js/login-column.js
+++ app/code/AgSoftware/Login/view/frontend/web/js/login-column.js
@@ -10,27 +10,15 @@ define([
            },
            getFullName: function () {
                if(location.pathname == "checkout"){
-                   
                    return false;
                }
                try {
                    let out = this.customer().fullname;
                    if(out) {
-                       $('.page-wrapper').attr("style",'margin-left:8%;'); 
                        $('#tab-mini-cart.showcart').show();
                        $('#login-column.display-sidebar-login').show();
                        $('body').addClass('logged');
                        return out;
-                   }
-                   else {
-                        $('.page-wrapper').attr("style",'margin-left:0px;'); 
-                   }
-                   let desp = $('.content-column-login:hover');
-                   if (desp) {
-                        $('.page-wrapper').attr("style",'margin-left:20%;'); 
-                   }
-                   else  {
-                    $('.page-wrapper').attr("style",'margin-left:8%;'); 
                    }
                }catch (e) {}
                $('body').removeClass('logged');
